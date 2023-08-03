51b86cebbffae74ddd98a800ae59c3700a90e2ea
Rohan Bansal <rohan@parsimony.com>
2023-08-03 12:59:58 +0530

    fix: setup saml request

--- saml/auth.py
+++ saml/auth.py
@@ -1,5 +1,9 @@
+from pathlib import Path
+
 import frappe
-import saml2 # not clear that this is the right solution
+from onelogin.saml2.auth import OneLogin_Saml2_Auth
+from onelogin.saml2.settings import OneLogin_Saml2_Settings
+from onelogin.saml2.utils import OneLogin_Saml2_Utils
 
 """
 Resources
@@ -19,19 +23,34 @@ https://www.youtube.com/watch?v=l-6QSEqDJPo
 
 
 def authenticate(*args, **kwargs):
-	# if user is logged in, don't authenticate 
-	print('saml.authenticate')
-	print(args)
-	print(kwargs)
+	# if user is logged in, don't authenticate
+	base_path = Path(frappe.get_app_path("saml")) / "saml"
+	request = {
+		"http_host": frappe.request.host_url,
+		"script_name": frappe.request.path,
+		"get_data": frappe.request.get_data(),
+		# 'post_data': request.POST.copy()
+	}
+
+	auth = OneLogin_Saml2_Auth(request, custom_base_path=str(base_path))
+	saml_settings = auth.get_settings()
+	metadata = saml_settings.get_sp_metadata()
+	errors = saml_settings.validate_metadata(metadata)
+
+	if errors:
+		frappe.throw("Invalid SP metadata: %s" % (", ".join(errors)))
+
+	saml_request_path = auth.login(return_to=frappe.request.host_url + "/app")
+	print(saml_request_path)
 
 
 """
 Assertion Consumer Service endpoint
 """
+
+
 @frappe.whitelist()
 def login_with_saml2(*args, **kwargs):
-	print('saml.login_with_saml2')
+	print("saml.login_with_saml2")
 	print(args)
 	print(kwargs)
-
-

83e65e136a10a964762879378451558457d5f853
Rohan Bansal <rohan@agritheory.dev>
2025-03-26 14:59:08 +0530

    feat: map SAML role to a Frappe user role

--- saml/saml/__init__.py
+++ saml/saml/__init__.py
@@ -3,42 +3,42 @@
 
 import frappe
 from frappe import _
-from onelogin.saml2.auth import OneLogin_Saml2_Auth
+from onelogin.saml2.auth import OneLogin_Saml2_Auth, OneLogin_Saml2_Settings
 
 
 @frappe.whitelist(allow_guest=True)
 def login(provider):
-	redirect_location = frappe.local.request.args.get("redirect-to", "")
-	# Fetch the SAML provider settings
 	saml_key = frappe.get_doc("SAML Login Key", provider)
-	# Create the SAML settings dictionary
-	saml_settings = {
-		"sp": {
-			"entityId": saml_key.sp_entity_id,
-			"assertionConsumerService": {
-				"url": frappe.utils.get_url(f"/api/method/saml.saml.acs?provider={provider}"),
-				"binding": "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST",
+	saml_settings = OneLogin_Saml2_Settings(
+		{
+			"sp": {
+				"entityId": saml_key.sp_entity_id,
+				"assertionConsumerService": {
+					"url": frappe.utils.get_url(f"/api/method/saml.saml.acs?provider={provider}"),
+					"binding": "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST",
+				},
+				"privateKey": saml_key.get_password("sp_private_key"),
+				"x509cert": saml_key.sp_x509cert,
 			},
-			"privateKey": saml_key.get_password("sp_private_key"),
-			"x509cert": saml_key.sp_x509cert,
-		},
-		"idp": {
-			"entityId": saml_key.idp_entity_id,
-			"singleSignOnService": {
-				"url": saml_key.idp_sso_url,
-				"binding": "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect",
+			"idp": {
+				"entityId": saml_key.idp_entity_id,
+				"singleSignOnService": {
+					"url": saml_key.idp_sso_url,
+					"binding": "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect",
+				},
+				"x509cert": saml_key.idp_x509cert,
 			},
-			"x509cert": saml_key.idp_x509cert,
-		},
-		"security": {
-			"authnRequestsSigned": True,  # Sign SAML authentication requests
-			"signatureAlgorithm": "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256",
-			"digestAlgorithm": "http://www.w3.org/2001/04/xmlenc#sha256",
-			"rejectUnsolicitedResponsesWithInResponseTo": False,
-		},
-	}
+			"security": {
+				"authnRequestsSigned": True,  # Sign SAML authentication requests
+				"signatureAlgorithm": "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256",
+				"digestAlgorithm": "http://www.w3.org/2001/04/xmlenc#sha256",
+				"rejectUnsolicitedResponsesWithInResponseTo": False,
+			},
+		}
+	)
 
 	client = OneLogin_Saml2_Auth(get_request_data(provider), saml_settings)
+	redirect_location = frappe.local.request.args.get("redirect-to", "")
 	redirect_url = client.login(return_to=redirect_location)
 	frappe.local.response["type"] = "redirect"
 	frappe.local.response["location"] = redirect_url
@@ -70,33 +70,37 @@ def acs():
 		}
 
 		saml_key = frappe.get_doc("SAML Login Key", query_data.get("provider"))
+		saml_settings = OneLogin_Saml2_Settings(
+			{
+				"debug": True,
+				"sp": {
+					"entityId": saml_key.sp_entity_id,
+					"assertionConsumerService": {
+						"url": frappe.utils.get_url("/api/method/saml.saml.acs"),
+						"binding": "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST",
+					},
+					"signatureAlgorithm": "RSA_SHA256",
+				},
+				"idp": {
+					"entityId": saml_key.idp_entity_id,
+					"singleSignOnService": {
+						"url": saml_key.idp_sso_url,
+						"binding": "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect",
+					},
+					"x509cert": saml_key.idp_x509cert,
+				},
+			}
+		)
 
-		# Create the SAML settings dictionary
-		saml_settings = {
-			"sp": {
-				"entityId": saml_key.sp_entity_id,
-				"assertionConsumerService": {
-					"url": frappe.utils.get_url("/api/method/saml.saml.acs"),
-					"binding": "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST",
-				},
-				"signatureAlgorithm": "RSA_SHA256",
-			},
-			"idp": {
-				"entityId": saml_key.idp_entity_id,
-				"singleSignOnService": {
-					"url": saml_key.idp_sso_url,
-					"binding": "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect",
-				},
-				"x509cert": saml_key.idp_x509cert,
-			},
-		}
 		client = OneLogin_Saml2_Auth(request_data, saml_settings)
 		client.process_response()
 
 		errors = client.get_errors()
 		if errors:
 			frappe.respond_as_web_page(
-				_("SAML Login Failed"), _("Invalid SAML response: ") + ", ".join(errors), http_status_code=403
+				_("SAML Login Failed"),
+				_("Invalid SAML response: ") + ", ".join(errors),
+				http_status_code=403,
 			)
 			return
 
@@ -105,37 +109,62 @@ def acs():
 				_("SAML Login Failed"), _("User not authenticated"), http_status_code=403
 			)
 			return
+
+		# Get user details
+		friendly_name = client.get_friendlyname_attributes()
 		attributes = client.get_attributes()
-		first_name = attributes.get("firstName", [None])[0]
-		last_name = attributes.get("lastName", [None])[0]
+		if friendly_name:
+			first_name = friendly_name.get("givenName", [None])[0]
+			last_name = friendly_name.get("surname", [None])[0]
+		elif attributes:
+			first_name = attributes.get("firstName", [None])[0]
+			last_name = attributes.get("lastName", [None])[0]
+		else:
+			first_name = last_name = None
+
 		user_email = client.get_nameid()
 		if not user_email:
 			frappe.respond_as_web_page(
 				_("SAML Login Failed"), _("Email not found in SAML response"), http_status_code=403
 			)
 			return
+
 		user = frappe.db.get_value("User", {"email": user_email})
 		if not user:
 			if not first_name:
 				first_name = user_email
 			user = frappe.new_doc("User")
-			user.first_name = first_name
-			user.last_name = last_name
-			user.email = user_email
-			user.enabled = 1
+			user.update(
+				{
+					"enabled": True,
+					"email": user_email,
+					"first_name": first_name,
+					"last_name": last_name,
+					"send_welcome_email": False,
+				}
+			)
 			user.flags.ignore_permissions = True
-			user.flags.no_welcome_mail = True
+			user.insert()
+
 			if default_role := frappe.db.get_single_value("Portal Settings", "default_role"):
 				user.add_roles(default_role)
-			user.save()
 		else:
 			user = frappe.get_doc("User", user_email)
+
+		# Map SAML roles
+		roles = attributes.get("Role", [])
+		for role in roles:
+			for role_mapping in saml_key.roles:
+				if role_mapping.saml_role == role:
+					user.flags.ignore_permissions = True
+					user.add_roles(role_mapping.user_role)
 
 		# Log the user in
 		frappe.local.login_manager.user = user.name
 		frappe.local.login_manager.post_login()
 		frappe.db.commit()
 		redirect_to = post_data.get("RelayState")
+
 		# Default to /me for website users or /app for desk users
 		if not redirect_to:
 			redirect_to = "/me" if user.user_type == "Website User" else "/app"
